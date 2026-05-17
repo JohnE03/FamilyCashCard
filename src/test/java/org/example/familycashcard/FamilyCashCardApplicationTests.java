ffe0d0632713bc750e0e6d60ea7957d36e8b3807
JohnE03 <2101519@eng.asu.edu.eg>
2026-05-17 19:48:16 +0300

    Family Cash Card project creation

---
+++ src/test/java/org/example/familycashcard/FamilyCashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.familycashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyCashCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
