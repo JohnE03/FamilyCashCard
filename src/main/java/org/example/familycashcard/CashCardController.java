5fe05a451fe546cc3310b31f0f3763d342cd3ad4
JohnE03 <2101519@eng.asu.edu.eg>
2026-05-18 22:22:53 +0300

    Test Driven Development 4 - Added pagination and sorting to CashCardRepository and REST endpoint, implemented createCashCard functionality, and updated tests accordingly

--- src/main/java/org/example/familycashcard/CashCardController.java
+++ src/main/java/org/example/familycashcard/CashCardController.java
@@ -1,11 +1,16 @@
 package org.example.familycashcard;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -20,12 +25,26 @@ class CashCardController {
     @GetMapping("/{id}")
     private ResponseEntity<CashCard> findById(@PathVariable Long id) {
         Optional<CashCard> cashCard = cashCardRepository.findById(id);
-        if(cashCard.isPresent()){
-            return ResponseEntity.ok(cashCard.get());
-        }
-        else{
-            return ResponseEntity.notFound().build();
-        }
+        return cashCard.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @PostMapping()
+    private ResponseEntity<Void> create(@RequestBody CashCard cashCard, UriComponentsBuilder ucb){
+        CashCard savedCashCard = cashCardRepository.save(cashCard);
+        URI locationOfNewCashCard = ucb.path("cashcards/{id}")
+                .buildAndExpand(savedCashCard.id())
+                .toUri();
+        return ResponseEntity.created(locationOfNewCashCard).build();
+    }
+
+    @GetMapping()
+    private ResponseEntity<List<CashCard>> findAll(Pageable pageable){
+        Page<CashCard> page = cashCardRepository.findAll(
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize(),
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+                ));
+        return ResponseEntity.ok(page.getContent());
+    }
 }
